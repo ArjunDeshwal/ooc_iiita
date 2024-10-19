cc2eca624fba34e685160327fd4b968d43141d04
ArjunDeshwal <arjundeshwal@hotmail.com>
2024-10-19 19:39:20 +0530

    ...

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let text=document.querySelector("#nav h4")
-let anim_nav=gsap.to("#nav h4", {
-    
-})
+
 gsap.to("#nav",{
     backgroundColor:"#000",
     duration:0.2,
@@ -9,21 +6,9 @@ gsap.to("#nav",{
     scrollTrigger:{
         trigger:"#nav",
         scroller:"body",
-        markers:true,
         start:"top -10%",
         end:"top -11%",
         scrub:3
     }
 
-})
-gsap.to("#main", {
-    backgroundColor:"#000",
-    scrollTrigger:{
-        trigger:"#main",
-        scroller:"body",
-        markers:true,
-        start:"top -75%",
-        end:"top -80%",
-        scrub:2,
-    }
 })
